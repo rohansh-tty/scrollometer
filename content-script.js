a1d1c07743d4592d960544f65492385becc0042f
Gilf641 <shettyrohan641@yahoo.com>
2021-11-24 19:33:11 +0530

    First commit scrollometer

--- content-script.js
+++ content-script.js
@@ -1,4 +1,6 @@
 var scrollLength = [0] 
+var myHeaders = new Headers();
+
 
 document.addEventListener('scroll', function(){
 
@@ -14,13 +16,43 @@ document.addEventListener('scroll', function(){
     lastElement = scrollLength.slice(-1)[0];
     // if currentElement > lastElement, only then add it to array
     if (scrollTop > lastElement){
+        // this is to make sure that the scroll stat doesn't reduce on scrolling up*
         scrollLength.push(scrollTop);
     }
     max_value = Math.max.apply(Math, scrollLength);
     scrollLength_in_mtrs = max_value * 0.0002645833;
+    
+    host = window.location.hostname;
+    console.log('host', host);
+
+    // var timestamp = Date(new Date()); // current time as number
 
 
-    // console.log('percent', percent);
+    var stats_data = [[1, 20, host, scrollLength_in_mtrs]]
+    // // next would be to show the scroll stats realtime on popup page.
+
+    // // POST scroll data back to Google Sheet
+    // var myHeaders = new Headers();
+    // myHeaders.append("Content-Type", "application/json");
+    // var requestOptions = {
+    //     method: "post",
+    //     headers: myHeaders,
+    //     redirect: "follow",
+    //     body: JSON.stringify(stats_data)
+    // };
+    
+    // fetch("https://v1.nocodeapi.com/gilf641/google_sheets/lLlefmIJOSPuLxtd?tabId=Sheet1", requestOptions)
+    //     .then(response => response.text())
+    //     .then(result => console.log(result))
+    //     .catch(error => console.log('error', error));
+
+
     console.log('Scrollometer:->>>', scrollLength_in_mtrs, 'meters');
 		
 });
+
+
+
+// xano endpoint
+let xanoURL = new this.URL();
+
---
+++ index.js
@@ -0,0 +1,22 @@
+import supabase from "./lib/supabase";
+
+
+async function insertRow(){
+    const { data, error } = await supabase
+    .from('test')
+    .insert([
+    { host: 'youtube', scroll: 2 },
+    ])
+}
+
+async function getRow(){
+    let { data: test, error } = await supabase
+    .from('test')
+    .select('*')
+}
+
+async function getStats(){
+    let { data: test, error } = await supabase
+    .from('test')
+    .select('some_column,other_column')
+}
