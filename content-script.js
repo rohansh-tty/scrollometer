5f286d57d5235970fb0a5dca397fb9fac46513b7
Gilf641 <shettyrohan641@yahoo.com>
2021-11-30 00:22:26 +0530

    fixed speed & scroll, need to check time spent

--- content-script.js
+++ content-script.js
@@ -1,10 +1,6 @@
 var scrollLength = [0] 
 var myHeaders = new Headers();
 
-
-// const { performance } = require('perf_hooks');
-
-
 document.addEventListener('scroll', function(){
 
     var h = document.documentElement, 
@@ -12,11 +8,11 @@ document.addEventListener('scroll', function(){
         st = 'scrollTop',
         sh = 'scrollHeight';
 
-    console.log('st, sh', st, sh);
-    var startTime = performance.now()
+    // var startTime = performance.now()
+     // enter your custom date in the Date() function.
+
     var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
     lastElement = scrollLength.slice(-1)[0];
-    // if currentElement > lastElement, only then add it to array
     if (scrollTop > lastElement){
         // this is to make sure that the scroll stat doesn't reduce on scrolling up*
         scrollLength.push(scrollTop);
@@ -25,26 +21,35 @@ document.addEventListener('scroll', function(){
     scroll_in_mtrs = max_value * 0.0002645833;
     
     host = window.location.hostname;
-    console.log('host', host);
 
-    // var timestamp = Date(new Date()); // current time as number
+// WILL HAVE TO FIX TIME ISSUE!!!  //
 
 
-    var endTime = performance.now();
-    time_spent = endTime - startTime;
+    // only if the page gets reloaded time reloadds
+    if (performance.getEntriesByType('navigation')[0].type == 'reload') {
+        console.info( "This page is reloaded" );
+        var customStartTime = new Date()
+
+        // var endTime = performance.now();
+        // time_spent = endTime - startTime;
+    
+      } else {
+        console.info( "This page is not reloaded");
+      }
+      
+    startTime = customStartTime || parseInt(localStorage.getItem('startTime') || Date.now());
+   
+    speed_ = scroll_in_mtrs/startTime;
+
     // // next would be to show the scroll stats realtime on popup page.
 
   
     console.log('Scrollo:->>>', scroll_in_mtrs, 'meters');
-    // console.log('speed:->>>', scroll_in_mtrs/time_spent, 'mtrs/sec');
-    // console.log('time:->>>', time_spent, 'ms');
+    console.log('Time spent>>>> ', startTime);
+  
 
-    chrome.runtime.sendMessage({scroll: scroll_in_mtrs}, (response)=>{
+    chrome.runtime.sendMessage({scroll: scroll_in_mtrs, time: startTime, speed: speed_}, (response)=>{
         console.log('scrollength-content_script.js', response);
     })
-
-
-    
-    print('updateing')
 
 });
--- popup.js
+++ popup.js
@@ -4,15 +4,23 @@ console.log('Inside popup.js');
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse){
     scroll_ = message.scroll;
-    // scroll_ = scrollValue.slice(-1)[0]
-    // speed = message.speed;
-    // time = message.time;
+    speed_ = message.speed;
+    time_ = message.time;
+    
+ 
 
     console.log('scroll length-popup', scroll_ ) ;
+    console.log('speed popup', speed_);
+    console.log('time popup', time_);
+
 
     // max_value = Math.max.apply(Math, scroll_);
     // scroll_in_mtrs = max_value * 0.0002645833;/
-    document.getElementById('scroll').innerHTML = Math.round(scroll_);
+    document.getElementById('scroll').innerHTML = scroll_.toFixed(4);
+    document.getElementById('speed').innerHTML = speed_.toFixed(4);
+    document.getElementById('time').innerHTML = time_.toFixed(4);
+
+
     // document.getElementById('time').innerHTML = time;
     // document.getElementById('speed').innerHTML = speed;
 
