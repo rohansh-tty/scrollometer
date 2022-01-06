298eab70dafb9f751dda150a3611c0dfc2b22045
rohanshetty-dev <rohanshetty@machanirobotics.com>
2022-01-06 07:37:52 +0530

    updated popup fonts & working on timer feature

--- content-script.js
+++ content-script.js
@@ -3,19 +3,70 @@ var myHeaders = new Headers();
 var timeStampArray = [0];
 
 var maxScrollY = 0;
+console.log("Inside popup.js");
+window.onload = () => {
+  
+  let hour = 0;
+  let minute = 0;
+  let seconds = 0;
+  let totalSeconds = 0;
+
+  let intervalId = null;
+
+  intervalId = setInterval(startTimer, 1000);
+  function startTimer() {
+    ++totalSeconds;
+    hour = Math.floor(totalSeconds / 3600);
+    minute = Math.floor((totalSeconds - hour * 3600) / 60);
+    seconds = totalSeconds - (hour * 3600 + minute * 60);
+
+    // document.getElementById("hour").innerHTML = hour;
+    console.log('Total Minutes spent', minute, seconds);
+  }
+
+
+    const time_box_element =document.getElementById("time-box-value");
+    time_box_element.innerHTML = minute + "minutes" + seconds + "seconds";
+
+    var time_dict = {hour: hour, minute: minute, seconds: seconds};
+    var time_array = [hour, minute, seconds];
+    chrome.runtime.sendMessage({"type": "time_spent", time_taken: time_dict}, (time_dict) => {
+      console.log("time_dict", time_dict);
+    })
+  
+}
+
 
 const supabaseClient = supabase.createClient(
   "https://iwnluhqtlcmaxrgbkyrk.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzA4NDYxOCwiZXhwIjoxOTUyNjYwNjE4fQ.6OfnqomnPQUwbuXYs_T9IwZ3qfSRzVFzyTD9pFfrp6I"
 );
 
+  
+    
+
 document.addEventListener("scroll", function () {
   console.log("[CS] Scrolling..");
-
   let scrollY = window.scrollY;
   maxScrollY = Math.max(scrollY, maxScrollY);
-
   let host = window.location.hostname;
+
+  // Timer
+  let hour = 0;
+  let minute = 0;
+  let seconds = 0;
+  let totalSeconds = 0;
+  let intervalId = null;
+
+  intervalId = setInterval(startTimer, 1000);
+  function startTimer() {
+    ++totalSeconds;
+    hour = Math.floor(totalSeconds / 3600);
+    minute = Math.floor((totalSeconds - hour * 3600) / 60);
+    seconds = totalSeconds - (hour * 3600 + minute * 60);
+  }
+  console.log('time array', hour, minute, seconds);
+
 
   chrome.storage.local.get("scroll", async function (result) {
 
@@ -45,9 +96,14 @@ document.addEventListener("scroll", function () {
     scroll_value = data[0].scroll_value;
     host_name = data[0].host;
 
-    chrome.runtime.sendMessage({scroll: scroll_value, host: host_name}, (scroll_value) => {
-      console.log("scroll value", scroll_value)
+    chrome.runtime.sendMessage({"type": "scroll", scroll: scroll_value, host: host_name}, (scroll_value, host_name) => {
+      console.log("scroll value & host name", scroll_value, host_name);
     })
+
+    chrome.runtime.sendMessage({"type": "time_spent", time_taken: seconds}, (seconds) => {
+      console.log("time_dict", seconds);
+    })
+  
 
     if (error) console.log("error: ", select_error);
   });
--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-console.log("Inside popup.js");
-
 
 chrome.storage.local.get(["scroll"], function (result) {
   console.log("result: ", result.scroll);
@@ -21,6 +19,7 @@ chrome.storage.local.get(["scroll"], function (result) {
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
   const scroll_element = document.getElementById('scroll-value');
   const host_element = document.getElementById('host-name');
+  const time_box_element = document.getElementById('time-box-value');
   console.log('host element', host_element);
 
   
@@ -28,13 +27,20 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
   // console.log('scroll value from CS', message.scroll);
   console.log('host name from CS', message.host);
 
-  scroll_element.innerHTML = message.scroll;
-  host_element.innerHTML = message.host;
-
-
-
+  if (message.type == "scroll") {
+    console.log('message type is scroll');
+    scroll_element.innerHTML = message.scroll;
+    host_element.innerHTML = message.host;}
+  else if (message.type == "time_spent"){
+    console.log('message type is time spent');
+    seconds = message.time_taken.seconds;
+    console.log('seconds', seconds);
+    time_box_element.innerHTML = seconds;
+  }
   
 
-  console.log(sender);
+  
+  
+  console.log('sender', sender);
   }
 );
