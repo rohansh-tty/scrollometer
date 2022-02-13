3fdae76323c11eca7a72f98cb6cabbad3a8a3358
AbuAbdhullah <abu.cbe92@gmail.com>
2022-02-13 21:25:15 +0530

    Modifying the content script and popup pages according to new UI design

--- content-script.js
+++ content-script.js
@@ -10,8 +10,18 @@ const supabaseClient = supabase.createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzA4NDYxOCwiZXhwIjoxOTUyNjYwNjE4fQ.6OfnqomnPQUwbuXYs_T9IwZ3qfSRzVFzyTD9pFfrp6I"
 );
 
-  
-    
+supabaseClient.auth.onAuthStateChange((event, session) => {
+  if ("SIGNED_IN" === event) {
+    console.log("Signed in....", supabaseClient.auth.user());
+
+    chrome.storage.local.set({ user_id_sm: session.user.id });
+    chrome.storage.local.set({ session_expiry: session.expires_at * 1000 });
+  } else if ("SIGNED_OUT" === event) {
+    console.log("Signed out....");
+
+    chrome.storage.local.set({ user_id_sm: null });
+  }
+});
 
 document.addEventListener("scroll", function () {
   console.log("[CS] Scrolling..");
@@ -19,57 +29,78 @@ document.addEventListener("scroll", function () {
   maxScrollY = Math.max(scrollY, maxScrollY);
   let host = window.location.hostname;
 
+  console.log("host: ", host);
 
-  chrome.storage.local.get("scroll", async function (result) {
-
+  chrome.storage.local.get(["scroll", "user_id_sm"], async function (result) {
     let obj = result.scroll;
     obj[host] = maxScrollY;
     chrome.storage.local.set({ scroll: obj });
 
     var date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
 
-    const { data, error } = await supabaseClient.from("Scroll").upsert([
-      {
-        session_id: host + "#" + date,
-        host: host,
-        scroll_value: maxScrollY,
-      },
-    ]);
-    if (error) console.log("error: ", error);
-     
-    let { data: Scroll, select_error } = await supabaseClient.from('Scroll').select('*').eq('host', host);
-    scroll_value = data[0].scroll_value;
-    host_name = data[0].host;
+    console.log("result.user_id_sm: ", result.user_id_sm);
 
-    
-    // get the start time, concat date with time
-    start_time = data[0].created_at;
-    session_id = data[0].session_id;
-    date = session_id.split('#')[1];
-    start_datetime = date.concat(' ', start_time);
+    if (result.user_id_sm) {
+      console.log("date: ", date);
 
-    // get current date, concat date with time, using UTC time for easy calculations
-    var curr_date = new Date();
-    // to subtract timezone offset
-    curr_date.setMinutes(curr_date.getMinutes() + curr_date.getTimezoneOffset());
+      const { data, error } = await supabaseClient.from("Scroll").upsert([
+        {
+          session_id: host + "#" + date,
+          user_id: result.user_id_sm,
+          host: host,
+          scroll_value: maxScrollY,
+          created_date: date,
+        },
+      ]);
+      // if (data) console.log("data: ", data);
+      if (error) console.log("error: ", error);
+    } else {
+      // TODO: Handle this case
+    }
 
-    var dmy = curr_date.getDate() + '/' + curr_date.getMonth() +'/'+ curr_date.getYear();
-    var curr_datetime = curr_date.toString();
-  
-    // to convert str to Date format
-    var current = new Date(curr_datetime);
-    var start = new Date(start_datetime);
+    // let { data: Scroll, select_error } = await supabaseClient
+    //   .from("Scroll")
+    //   .select("*")
+    //   .eq("host", host);
 
+    // // console.log("data: ", Scroll);
 
-    // get values in hh:mm:ss format
-    var milliseconds = current.getTime() - start.getTime();
-   
+    // scroll_value = Scroll[0].scroll_value;
+    // host_name = Scroll[0].host;
 
-    if (error) console.log("error: ", select_error);
+    // // get the start time, concat date with time
+    // start_time = Scroll[0].created_at;
+    // session_id = Scroll[0].session_id;
+    // date = session_id.split("#")[1];
+    // start_datetime = date.concat(" ", start_time);
 
-    chrome.runtime.sendMessage({scroll: scroll_value, host: host_name, time_spent: milliseconds }, (scroll_value, host_name) => {
-    })
+    // // get current date, concat date with time, using UTC time for easy calculations
+    // var curr_date = new Date();
+    // // to subtract timezone offset
+    // curr_date.setMinutes(
+    //   curr_date.getMinutes() + curr_date.getTimezoneOffset()
+    // );
 
+    // var dmy =
+    //   curr_date.getDate() +
+    //   "/" +
+    //   curr_date.getMonth() +
+    //   "/" +
+    //   curr_date.getYear();
+    // var curr_datetime = curr_date.toString();
+
+    // // to convert str to Date format
+    // var current = new Date(curr_datetime);
+    // var start = new Date(start_datetime);
+
+    // // get values in hh:mm:ss format
+    // var milliseconds = current.getTime() - start.getTime();
+
+    // if (select_error) console.log("error: ", select_error);
+
+    // chrome.runtime.sendMessage(
+    //   { scroll: scroll_value, host: host_name, time_spent: milliseconds },
+    //   (scroll_value, host_name) => {}
+    // );
   });
 });
-
--- popup.js
+++ popup.js
@@ -1,52 +1,99 @@
+const supabaseClient = supabase.createClient(
+  "https://iwnluhqtlcmaxrgbkyrk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzA4NDYxOCwiZXhwIjoxOTUyNjYwNjE4fQ.6OfnqomnPQUwbuXYs_T9IwZ3qfSRzVFzyTD9pFfrp6I"
+);
 
-function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-}
+chrome.storage.local.get("user_id_sm", async ({ user_id_sm }) => {
+  if (user_id_sm) {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      async function (tabs) {
+        let { host } = new URL(tabs[0].url);
+        console.log("host: ", tabs);
 
+        const { data, error } = await supabaseClient
+          .from("Scroll")
+          .select()
+          .eq("user_id", user_id_sm)
+          .eq("host", host);
 
-chrome.storage.local.get(["scroll"], function (result) {
-  console.log("result in popup.js: ", result.scroll);
-  let values = Object.values(result.scroll);
+        const endTime = new Date();
+        const startTime = new Date(data[0].created_time);
+        const totalTime = endTime - startTime;
 
-  // getting the value one time need to fix with realtime solution
-  chrome.tabs.query(
-    { active: true, currentWindow: true },
-    async function (tabs) {
-      let url = new URL(tabs[0].url);
-    }
-  );
+        const speed = data[0].scroll_value * 2.6458335e-7;
+
+        const scroll_element = document.getElementById("scroll-value");
+        const speed_element = document.getElementById("speed-value");
+        const host_element = document.getElementById("host-name");
+        const time_box_element = document.getElementById("time-value");
+
+        scroll_element.innerHTML = data[0].scroll_value;
+        host_element.innerHTML = host;
+        time_box_element.innerHTML = msToTime(totalTime);
+        speed_element.innerHTML = round(speed, 2);
+      }
+    );
+  }
 });
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-  const scroll_element = document.getElementById('scroll-value');
-  const speed_element = document.getElementById('speed-value');
-  const host_element = document.getElementById('host-name');
-  const time_box_element = document.getElementById('time-value');
-  
-  var milliseconds = message.time_spent;
+function msToTime(duration) {
+  let seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  var hh = Math.floor(milliseconds / 1000 / 60 / 60);
-  milliseconds -= hh * 1000 * 60 * 60;
-  var mm = Math.floor(milliseconds / 1000 / 60);
-  milliseconds -= mm * 1000 * 60;
-  var ss = Math.floor(milliseconds / 1000);
-  milliseconds -= ss * 1000;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
+  return hours + ":" + minutes + ":" + seconds;
+}
 
-   //speed caluclation
-   total_seconds = mm*60 + ss;
-   speed = round(message.scroll/total_seconds, 3);
+function round(value, decimals) {
+  return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+}
 
+// chrome.storage.local.get(["scroll"], function (result) {
+//   console.log("result in popup.js: ", result.scroll);
+//   let values = Object.values(result.scroll);
 
-  var pad_hh = hh.toString().padStart(2, '0');
-  var pad_mm = mm.toString().padStart(2, '0');
-  var pad_ss = ss.toString().padStart(2, '0');
+//   // getting the value one time need to fix with realtime solution
+//   chrome.tabs.query(
+//     { active: true, currentWindow: true },
+//     async function (tabs) {
+//       console.log("tabs: ", tabs);
 
-  let currentTime = pad_hh + ":" + pad_mm + ":" + pad_ss;
-  scroll_element.innerHTML = message.scroll;
-  speed_element.innerHTML = speed;
-  host_element.innerHTML = message.host;
-  time_box_element.innerHTML = currentTime;
+//       let url = new URL(tabs[0].url);
+//     }
+//   );
+// });
 
-  }
-);
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   const scroll_element = document.getElementById("scroll-value");
+//   const speed_element = document.getElementById("speed-value");
+//   const host_element = document.getElementById("host-name");
+//   const time_box_element = document.getElementById("time-value");
+
+//   var milliseconds = message.time_spent;
+
+//   var hh = Math.floor(milliseconds / 1000 / 60 / 60);
+//   milliseconds -= hh * 1000 * 60 * 60;
+//   var mm = Math.floor(milliseconds / 1000 / 60);
+//   milliseconds -= mm * 1000 * 60;
+//   var ss = Math.floor(milliseconds / 1000);
+//   milliseconds -= ss * 1000;
+
+//   //speed caluclation
+//   total_seconds = mm * 60 + ss;
+//   speed = round(message.scroll / total_seconds, 3);
+
+//   var pad_hh = hh.toString().padStart(2, "0");
+//   var pad_mm = mm.toString().padStart(2, "0");
+//   var pad_ss = ss.toString().padStart(2, "0");
+
+//   let currentTime = pad_hh + ":" + pad_mm + ":" + pad_ss;
+//   scroll_element.innerHTML = message.scroll;
+//   speed_element.innerHTML = speed;
+//   host_element.innerHTML = message.host;
+//   time_box_element.innerHTML = currentTime;
+// });
