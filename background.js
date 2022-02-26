8f7173bda66393ea73c877c08ccc6032ae15e097
AbuAbdhullah <abu.cbe92@gmail.com>
2022-02-26 20:51:50 +0530

    Extract user id from cookies

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
 chrome.storage.local.set({ scroll: {} });
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if ('set_user_id' === message) {
+        chrome.cookies.get({ "url": 'https://scrollometerui.vercel.app/', "name": 'user_id' }, function (cookie) {
+            console.log('cookie value: ', cookie.value)
+
+            chrome.storage.local.set({ user_id_sm: cookie.value });
+        });
+    }
+});
+
