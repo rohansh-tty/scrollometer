650647414c031a7a8015471fd4ee11e496a3f402
rohanshetty-dev <rohanshetty@machanirobotics.com>
2022-02-27 07:50:15 +0530

    formatted js & css

--- content-script.js
+++ content-script.js
@@ -6,66 +6,64 @@ var maxScrollY = 0;
 console.log("Inside content_script.js");
 
 const supabaseClient = supabase.createClient(
-  "https://iwnluhqtlcmaxrgbkyrk.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzA4NDYxOCwiZXhwIjoxOTUyNjYwNjE4fQ.6OfnqomnPQUwbuXYs_T9IwZ3qfSRzVFzyTD9pFfrp6I"
+    "https://iwnluhqtlcmaxrgbkyrk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzA4NDYxOCwiZXhwIjoxOTUyNjYwNjE4fQ.6OfnqomnPQUwbuXYs_T9IwZ3qfSRzVFzyTD9pFfrp6I"
 );
 
 supabaseClient.auth.onAuthStateChange((event, session) => {
-  if ("SIGNED_IN" === event) {
-    console.log("Signed in....", supabaseClient.auth.user());
+    if ("SIGNED_IN" === event) {
+        console.log("Signed in....", supabaseClient.auth.user());
 
-    chrome.storage.local.set({ user_id_sm: session.user.id });
-    chrome.storage.local.set({ session_expiry: session.expires_at * 1000 });
-  } else if ("SIGNED_OUT" === event) {
-    console.log("Signed out....");
+        chrome.storage.local.set({ user_id_sm: session.user.id });
+        chrome.storage.local.set({ session_expiry: session.expires_at * 1000 });
+    } else if ("SIGNED_OUT" === event) {
+        console.log("Signed out....");
 
-    chrome.storage.local.set({ user_id_sm: null });
-  }
+        chrome.storage.local.set({ user_id_sm: null });
+    }
 });
 
-document.addEventListener("scroll", function () {
-  console.log("[CS] Scrolling..");
-  let scrollY = window.scrollY;
+document.addEventListener("scroll", function() {
+    console.log("[CS] Scrolling..");
+    let scrollY = window.scrollY;
 
-  // making sure that reverse scrolling doesn't eat down
-  lastscroll = scrollLength.slice(-1)[0];
-  if (scrollY > lastscroll) {
-    scrollLength.push(scrollY);
-  }
-
-  console.log("scrollY", scrollY, "maxscrollY", maxScrollY);
-  // max can be Math.max(scrollY, maxScrollY) or Math.max.apply(Math, scrollLength)
-  maxScrollY = Math.max.apply(Math, scrollLength) * 0.2646;
-  scroll_value = Math.round(maxScrollY);
-  let host = window.location.hostname;
-
-  console.log("host: ", host);
-
-  chrome.storage.local.get(["scroll", "user_id_sm"], async function (result) {
-    let obj = result.scroll;
-    obj[host] = scroll_value; // 1inch=96px, 1inch=25.4mm, mm=px*(25.4/96)
-    chrome.storage.local.set({ scroll: obj });
-
-    var date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
-
-    console.log("result.user_id_sm: ", result.user_id_sm);
-
-    if (result.user_id_sm) {
-      console.log("date: ", date);
-
-      const { data, error } = await supabaseClient.from("Scroll").upsert([
-        {
-          session_id: host + "#" + date,
-          user_id: result.user_id_sm,
-          host: host,
-          scroll_value: maxScrollY,
-          created_date: date,
-        },
-      ]);
-      // if (data) console.log("data: ", data);
-      if (error) console.log("error: ", error);
-    } else {
-      // TODO: Handle this case
+    // making sure that reverse scrolling doesn't eat down
+    lastscroll = scrollLength.slice(-1)[0];
+    if (scrollY > lastscroll) {
+        scrollLength.push(scrollY);
     }
-  });
+
+    console.log("scrollY", scrollY, "maxscrollY", maxScrollY);
+    // max can be Math.max(scrollY, maxScrollY) or Math.max.apply(Math, scrollLength)
+    maxScrollY = Math.max.apply(Math, scrollLength) * 0.2646;
+    scroll_value = Math.round(maxScrollY);
+    let host = window.location.hostname;
+
+    console.log("host: ", host);
+
+    chrome.storage.local.get(["scroll", "user_id_sm"], async function(result) {
+        let obj = result.scroll;
+        obj[host] = scroll_value; // 1inch=96px, 1inch=25.4mm, mm=px*(25.4/96)
+        chrome.storage.local.set({ scroll: obj });
+
+        var date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+
+        console.log("result.user_id_sm: ", result.user_id_sm);
+
+        if (result.user_id_sm) {
+            console.log("date: ", date);
+
+            const { data, error } = await supabaseClient.from("Scroll").upsert([{
+                session_id: host + "#" + date,
+                user_id: result.user_id_sm,
+                host: host,
+                scroll_value: maxScrollY,
+                created_date: date,
+            }, ]);
+            // if (data) console.log("data: ", data);
+            if (error) console.log("error: ", error);
+        } else {
+            // TODO: Handle this case
+        }
+    });
 });
--- popup.js
+++ popup.js
@@ -1,54 +1,55 @@
 const supabaseClient = supabase.createClient(
-  "https://iwnluhqtlcmaxrgbkyrk.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzA4NDYxOCwiZXhwIjoxOTUyNjYwNjE4fQ.6OfnqomnPQUwbuXYs_T9IwZ3qfSRzVFzyTD9pFfrp6I"
+    "https://iwnluhqtlcmaxrgbkyrk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzA4NDYxOCwiZXhwIjoxOTUyNjYwNjE4fQ.6OfnqomnPQUwbuXYs_T9IwZ3qfSRzVFzyTD9pFfrp6I"
 );
 
-chrome.storage.local.get("user_id_sm", async ({ user_id_sm }) => {
-  if (user_id_sm) {
-    chrome.tabs.query(
-      { active: true, currentWindow: true },
-      async function (tabs) {
-        let { host } = new URL(tabs[0].url);
-        console.log("host: ", tabs);
+chrome.storage.local.get("user_id_sm", async({ user_id_sm }) => {
+    if (user_id_sm) {
+        chrome.tabs.query({ active: true, currentWindow: true },
+            async function(tabs) {
+                let { host } = new URL(tabs[0].url);
+                console.log("host: ", tabs);
 
-        const { data, error } = await supabaseClient
-          .from("Scroll")
-          .select()
-          .eq("user_id", user_id_sm)
-          .eq("host", host);
+                const { data, error } = await supabaseClient
+                    .from("Scroll")
+                    .select()
+                    .eq("user_id", user_id_sm)
+                    .eq("host", host);
 
-        const endTime = new Date();
-        const startTime = new Date(data[0].created_time);
-        const totalTime = endTime - startTime;
+                const endTime = new Date();
+                const startTime = new Date(data[0].created_time);
+                const totalTime = endTime - startTime;
 
-        const speed = data[0].scroll_value * 2.6458335e-7;
+                const speed = data[0].scroll_value * 2.6458335e-7;
 
-        const scroll_element = document.getElementById("scroll-value");
-        const speed_element = document.getElementById("speed-value");
-        const host_element = document.getElementById("host-name");
-        const time_box_element = document.getElementById("time-value");
+                const scroll_element = document.getElementById("scroll-value");
+                const speed_element = document.getElementById("speed-value");
+                const host_element = document.getElementById("host-name");
+                const time_box_element = document.getElementById("time-value");
 
-        scroll_element.innerHTML = data[0].scroll_value;
-        host_element.innerHTML = host;
-        time_box_element.innerHTML = msToTime(totalTime);
-        speed_element.innerHTML = round(speed, 2);
-      }
-    );
-  }
+                scroll_element.innerHTML = data[0].scroll_value;
+                host_element.innerHTML = host;
+                time_box_element.innerHTML = msToTime(totalTime);
+                speed_element.innerHTML = round(speed, 2);
+            }
+        );
+    } else {
+        console.log('login please.....')
+    }
 });
 
 function msToTime(duration) {
-  let seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    let seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  return hours + ":" + minutes + ":" + seconds;
+    return hours + ":" + minutes + ":" + seconds;
 }
 
 function round(value, decimals) {
-  return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+    return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
 }
